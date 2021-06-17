110ba385da2b1c0632d68c61676a9c735f39fde4
odaminov <daminov.new@gmail.com>
2021-06-18 01:40:07 +0300

    all 3 tasks

--- lessons/lesson2/hw_2.py
+++ lessons/lesson2/hw_2.py
@@ -91,29 +91,31 @@
 
 
 class Matrices:
-    def __init__(self,A,B):
+    def __init__(self, A, B):
         self.A=A
         self.B=B
     def display(self,C):
         print('Resultant Matrix is:')
-        for i in range(0,m):
+        for i in range(0, m):
             print('\n')
-            for j in range(0,n):
-                print(' {}'.format(C[i][j]),end=" ")
+            for j in range(0, n):
+                print(' {}'.format(C[i][j]), end=" ")
     def Add(self,C):
-        for i in range(0,m):
-            for j in range(0,n):
+        for i in range(0, m):
+            for j in range(0, n):
                 C[i][j] = A[i][j] + B[i][j]
-    def Sub(self,C):
-        for i in range(0,m):
-            for j in range(0,n):
+    def Sub(self, C):
+        for i in range(0, m):
+            for j in range(0, n):
                 C[i][j] = A[i][j] - B[i][j]
-    def Mul(self,C):
-        for i in range(0,m):
-            for j in range(0,q):
-                for k in range(0,n):
-                    C[i][j]+= A[i][k] * B[k][j]
-if __name__=='__main__':
+    def Mul(self, C):
+        for i in range(0, m):
+            for j in range(0, q):
+                for k in range(0, n):
+                    C[i][j] += A[i][k] * B[k][j]
+
+
+if __name__ == '__main__':
     m = int(input('Enter no. of rows for Matrix 1:'))
     n = int(input('Enter no. of columns for Matrix 1:'))
     A = [[0 for j in range(0, n)] for i in range(0, m)]
@@ -129,35 +131,35 @@ if __name__=='__main__':
         for j in range(0, q):
             B[i][j] = int(input('Enter element B{}{}:'.format(i, j)))
     obj = Matrices(A,B)
-    var =1
-    while var!='0':
+    var = 1
+    while var != '0':
         print('1.Add Matrices\n2.Subtract Matrices\n3.Multiply Matrices\n4.Exit')
         choice = int(input('Enter Choice:'))
-        if choice==1:
-            if m==p and n==q:
+        if choice == 1:
+            if m == p and n == q:
                 print('Matrices can be Added')
                 C = [[0 for j in range(0, n)] for i in range(0, m)]
                 obj.Add(C)
                 obj.display(C)
             else:
                 print('Matrices cannot be Added')
-        elif choice==2:
-            if m==p and n==q:
+        elif choice == 2:
+            if m == p and n == q:
                 print('Matrices can be Subtracted')
                 C = [[0 for j in range(0, n)] for i in range(0, m)]
                 obj.Sub(C)
                 obj.display(C)
             else:
                 print('Matrices cannot be Subtracted')
-        elif choice==3:
-            if n==p:
+        elif choice == 3:
+            if n == p:
                 print('Matrices can be Multiplied')
                 C = [[0 for j in range(0, q)] for i in range(0, m)]
                 obj.Mul(C)
                 obj.display(C)
             else:
                 print('Matrices cannot be Multiplied')
-        elif choice==4:
+        elif choice == 4:
             exit(0)
         else:
             print('\nPlease enter a valid choice')
